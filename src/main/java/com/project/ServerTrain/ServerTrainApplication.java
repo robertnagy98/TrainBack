b9efd091adc3fab405f27eca87d0edac875a5e89
Robert <robert.98.nagy@gmail.com>
2020-11-29 11:38:26 +0200

    First commit

---
+++ src/main/java/com/project/ServerTrain/ServerTrainApplication.java
@@ -0,0 +1,13 @@
+package com.project.ServerTrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerTrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerTrainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/ServerTrain/ServerTrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ServerTrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerTrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
